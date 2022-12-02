497dfc24a07f71206f99e3311b2b04348203375b
Jack Sprinkle <sprinkle.jas@gmail.com>
2022-12-01 19:17:20 -0500

    add header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import './Header.scss';
+import logo from '../../assets/images/google_logo.jpg';
+import help from '../../assets/icons/help.svg';
+import app from '../../assets/icons/apps.svg';
+
+function Header() {
+    return(
+        <div className='header'>
+            <div className='header__container--left'>
+                <img className='header__logo' src={logo} alt='google logo' />
+                <h2 className='header__text'>Account</h2>
+                <input className='header__search' type='text'></input>
+            </div>
+            <div className='header__container--right'>
+                <img className='header__help' src={help} alt='question mark inside of circle' />
+                <img className='header__app' src={app} alt='three by three grid of circles' />
+                <div className='header__avatar'></div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
