d0d51e21f537e8be68af07412c6ad732aa08590f
Jack Sprinkle <sprinkle.jas@gmail.com>
2022-12-02 01:05:08 -0500

    style modal

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,13 @@ import './Header.scss';
 import logo from '../../assets/images/google_logo.jpg';
 import help from '../../assets/icons/help.svg';
 import app from '../../assets/icons/apps.svg';
+import AccessModal from '../AccessModal/AccessModal';
+import {useState} from 'react';
 
 function Header() {
+
+    const [show, setShow] = useState(false);
+
     return(
         <div className='header'>
             <div className='header__container--left'>
@@ -12,7 +17,14 @@ function Header() {
                 <input className='header__search' type='text'></input>
             </div>
             <div className='header__container--right'>
-                <button className='header__access-button'></button>
+                <button 
+                    className='header__access-button'
+                    onClick={() => setShow(true)}
+                ></button>
+                <AccessModal 
+                    show={show}
+                    onClose={() => setShow(false)}
+                />
                 <img className='header__help' src={help} alt='question mark inside of circle' />
                 <img className='header__app' src={app} alt='three by three grid of circles' />
                 <div className='header__avatar'></div>
